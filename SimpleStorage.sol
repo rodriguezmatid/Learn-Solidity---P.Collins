8ad959f8ba3bb70b857ce1f88d8664c8cbb8df48
rodriguezmatid <matias.demian.r@gmail.com>
2023-11-16 20:18:11 -0300

    first contract

---
+++ SimpleStorage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    uint256 myFavoriteNumber;// 0
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256){
+        return myFavoriteNumber;
+    }
+
+    // calldata, memory, storage
+    // memory --> this variable will only exist during the duration of the function call that can be modify
+    // call data is also temporaly data that can not be modify
+    // permanent variables that can be modify
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
