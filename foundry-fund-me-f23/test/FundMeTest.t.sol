7ed8b8d05494f607a337b343ac9103e9a4e06640
rodriguezmatid <matias.demian.r@gmail.com>
2023-11-29 10:07:58 -0300

    refactoring fundme

--- foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
         // us -> FundMeTest -> FundMe
-        fundMe = new FundMe(); // a variable called fundMe of type FundMe will be a new contract FundMe
+        // fundMe = new FundMe(); // a variable called fundMe of type FundMe will be a new contract FundMe
+        DeployFundMe deployFundMe = new DeployFundMe(); // you are deploying a new contract
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -20,6 +23,17 @@ contract FundMeTest is Test {
         // Asegurarse de que i_owner es una función pública que devuelve una dirección
         address owner = fundMe.i_owner(); 
 
-        assertEq(owner, address(this)); // The owner is FundMeTest
+        assertEq(owner, msg.sender); // The owner is FundMeTest
+    }
+
+    // What can we do to work with addresses outside our system?
+    // 1. Unit. Testing a specific part of our code
+    // 2. Integration. Testing how our code works with other parts of our code
+    // 3. Forked. Testing our code on a simulated real environment
+    // 4. Testing our code in a real environment that is not prod
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
